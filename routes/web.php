f10fe3ce701424df58dd09b517d15c0e4b8bf237
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2025-09-25 02:00:02 +0200

    add middlewares to protect routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Route;
 // })->name('register');
 
 Route::view('/', 'posts.index')->name('home'); // Shorter syntax for simple routes
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::view('/register', 'auth.register')->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
